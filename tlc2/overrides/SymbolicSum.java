596c3afdd534d51cf7861ddf1197008583ecad8f
Theo Akande <ta722@ic.ac.uk>
2026-02-20 17:55:10 +0000

    feat: start of le for symbolic sum

--- tlc2/overrides/SymbolicSum.java
+++ tlc2/overrides/SymbolicSum.java
@@ -2,6 +2,7 @@ package tlc2.overrides;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 import tlc2.tool.FingerprintException;
 import tlc2.util.FP64;
@@ -14,9 +15,105 @@ public class SymbolicSum extends SymbolicExpression {
     private final Map<SymbolicExpression, Integer> bag = new HashMap<>();
     private int cardinality = 0;
     
-    public SymbolicSum(final Map<SymbolicExpression, Integer> bag) {
+    // Note: addTo is generally better to use (hence private)
+    private static SymbolicSum generate(final Map<SymbolicExpression, Integer> bag) {
+        final SymbolicSum newSum = new SymbolicSum(bag);
+        final SymbolicExpression oldSum = SymbolicExpression.get(newSum);
+        if (oldSum != null) {
+            return (SymbolicSum) oldSum;
+        }
+        newSum.setup();
+        return newSum;
+    } 
+
+    // setup a new symbolic max for le
+    private void setup() {
+        try {
+            final Set<SymbolicExpression> le = this.getAllLE();
+            final Set<SymbolicExpression> ge = this.getAllGE();
+            le.add(SymbolicEmpty.getInstance());
+            SymbolicEmpty.getInstance().setLessThan(this);
+            le.add(this);
+            ge.add(this);
+            final Set<SymbolicExpression> keys = this.bag.keySet();
+            for (final SymbolicExpression e : SymbolicExpression.getAll()) {
+                if (this.greaterThanWithoutCache(e)) {
+                    le.add(e);
+                    e.setLessThan(this);
+                } else if (this.lessThanWithoutCache(e)) {
+                    ge.add(e);
+                    e.setGreaterThan(this);
+                }
+            }
+            SymbolicExpression.addExpression(this);
+        } catch (final RuntimeException | OutOfMemoryError e) {
+            if (hasSource()) {throw FingerprintException.getNewHead(this, e);}
+            else {throw e;}
+        }
+    }
+
+    private SymbolicSum(final Map<SymbolicExpression, Integer> bag) {
         this.bag.putAll(bag);
         this.cardinality = bag.values().stream().mapToInt(Integer::intValue).sum();
+    }
+
+    private boolean greaterThanWithoutCache(final SymbolicExpression e) {
+        final Set<SymbolicExpression> keys = this.bag.keySet();
+        if (e.isEmpty()) {
+            return true;
+        } else if (e.isAtom()) {
+            for (final SymbolicExpression o : keys) {
+                if (o.getAllLE().contains(e)) {
+                    return true;
+                }
+            }
+        } else if (e.isMaxExpr()) {
+            final SymbolicMax m = (SymbolicMax) e;
+            return this.greaterThanWithoutCache(m.first()) && this.greaterThanWithoutCache(m.second());
+        } else if (e.isSumExpr()) {
+            // A sum should always be one more than an existing sum (known to be less); if not then this wont be known either
+            for (final SymbolicExpression o : e.getAllGE()) {
+                if (this.isOneGreater(o)) {
+                    return true;
+                }
+            }
+            // TODO: What if its not one more than an existing sum? How do i handle this waaaaaa
+        }
+        return false;
+    }
+
+    private boolean lessThanWithoutCache(final SymbolicExpression e) {
+        // TODO: Finish
+        return false;
+    }
+
+    private boolean isOneGreater(final SymbolicExpression o) {
+        if (o.isEmpty()) return false;
+        if (o.isAtom() || o.isMaxExpr()) return this.cardinality == 2 && this.bag.containsKey(o);
+        if (o.isSumExpr()) {
+            final SymbolicSum sumOther = (SymbolicSum) o;
+            if (sumOther.cardinality != this.cardinality - 1) {
+                return false;
+            }
+            boolean oneDifference = false;
+            for (final SymbolicExpression v : this.bag.keySet()) {
+                if (!sumOther.bag.containsKey(v)) {
+                    if (oneDifference || this.bag.get(v) > 1) {
+                        return false;
+                    }
+                    oneDifference = true;
+                } else {
+                    if (sumOther.bag.get(v) != this.bag.get(v)) {
+                        if (oneDifference) {
+                            return false;
+                        }
+                        oneDifference = true;
+                    }
+                }
+            }
+            return oneDifference;
+        }
+        return false;
     }
 
     protected Map<SymbolicExpression, Integer> getBag() {
@@ -35,7 +132,7 @@ public class SymbolicSum extends SymbolicExpression {
         try {
             final Map<SymbolicExpression, Integer> newBag = new HashMap<>(this.bag);
             newBag.put(e, newBag.getOrDefault(e, 0) + num);
-            return new SymbolicSum(newBag);
+            return SymbolicSum.generate(newBag);
         } catch (final RuntimeException | OutOfMemoryError err) {
             if (hasSource()) {throw FingerprintException.getNewHead(this, err);}
             else {throw err;}
@@ -48,7 +145,7 @@ public class SymbolicSum extends SymbolicExpression {
     @Override
     public IValue deepCopy() {
         try {
-            final SymbolicSum ret = new SymbolicSum(this.bag);
+            final SymbolicSum ret = SymbolicSum.generate(this.bag);
             return ret;
         } catch (final RuntimeException | OutOfMemoryError e) {
             if (hasSource()) {throw FingerprintException.getNewHead(this, e);}
@@ -87,7 +184,10 @@ public class SymbolicSum extends SymbolicExpression {
         try {
             if (other instanceof SymbolicSum) {
                 final SymbolicSum sumOther = (SymbolicSum) other;
-                return ((BoolValue)SymbolicExpression.equal(this, sumOther)).val;
+                if (sumOther.cardinality != this.cardinality) {
+                    return false;
+                }
+                return this.bag.equals(sumOther.bag);
             }
             return false;
         } catch (final RuntimeException | OutOfMemoryError e) {
